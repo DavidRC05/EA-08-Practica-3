eb254ec38dc93ba723746f2737e4bb8a62fe6fdf
DavidRC05 <105888182+DavidRC05@users.noreply.github.com>
2023-02-20 22:53:38 -0600

    Practica Creada

---
+++ Practica3/src/main/java/Practica3Grupo4/Practica3Grupo4Application.java
@@ -0,0 +1,13 @@
+package Practica3Grupo4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica3Grupo4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica3Grupo4Application.class, args);
+	}
+
+}
---
+++ Practica3/src/test/java/Practica3Grupo4/Practica3Grupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package Practica3Grupo4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica3Grupo4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
