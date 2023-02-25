6c626353445485a5476ac4143121892deba4e893
lawrence2807 <lawrencect28@gmail.com>
2023-02-24 20:16:31 -0600

    Modificaciones index.html, indexcontroller

--- src/main/java/Practica3Grupo4/controller/IndexController.java
+++ src/main/java/Practica3Grupo4/controller/IndexController.java
@@ -1,18 +1,26 @@
 
 package Practica3Grupo4.controller;
 
+import Practica3Grupo4.dao.EstadoDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
+@Slf4j
 @Controller
 public class IndexController {
     
-    @RequestMapping("/url")
+    @Autowired
+    EstadoDao estadoDao;
+            
+    @GetMapping("/")
     public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "view.name";
+        
+        var Estados = estadoDao.findAll();
+        model.addAttribute("Estados", Estados);
+        return "index";
     }
     
 }
