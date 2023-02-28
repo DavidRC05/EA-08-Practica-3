011eb45e858b034c8ad7848352921e4d7201bd1d
Juan Diego <101369290+juandiegocv27@users.noreply.github.com>
2023-02-28 02:54:24 -0600

    espero sirva, sino borre este commit, porque tengo problemas con el mysql

--- src/main/java/Practica3Grupo4/controller/IndexController.java
+++ src/main/java/Practica3Grupo4/controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -35,11 +36,10 @@ public class IndexController {
         return "redirect:/";
     }
 
-     @GetMapping("/modificarEstado/{idEstado}")
-    public String actualizarEstado(Estado estado, Model model){
-        estado = estadoService.getEstado(estado);
-        model.addAttribute("Estado", estado);
-        return "actualizar";
+     @GetMapping("/editar/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        model.addAttribute("estado", estadoService.buscarEstado(id));
+        return "form-estado";
     }
     
     @GetMapping("/eliminarEstado/{idEstado}")
